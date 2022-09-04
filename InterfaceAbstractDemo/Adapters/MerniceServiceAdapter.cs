8a48910400b934aad7481f7480b02a52fe2d6275
fatihatac <fatac2000@gmail.com>
2022-09-04 19:09:26 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Adapters/MerniceServiceAdapter.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReferance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Adapters
+{
+    public class MerniceServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            var result = client.TCKimlikNoDogrulaAsync(Convert.ToInt64(customer.NationalityId), customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year).GetAwaiter().GetResult();
+            return result.Body.TCKimlikNoDogrulaResult;
+
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MerniceServiceAdapter());
+            customerManager.Save(new Customer {Id = 1 , FirstName = "Fatih", LastName = "Ataç", DateOfBirth = new DateTime(2001,10,16), NationalityId = "28816736258" });
+        }
+    }
+}
